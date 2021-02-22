afc0b63c1a6f846656d027bce927c02536f416d4
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-02-22 18:27:32 +0100

    Added initial checkFirstTask function

--- app/src/app.js
+++ app/src/app.js
@@ -6,14 +6,31 @@ let path = __dirname + '/views/'
 // Use pug
 app.set('view engine', 'pug')
 
+// Handle static css and js files automatically
 app.use(express.static(__dirname))
 
+/*
+    Application Code
+ */
+
+// Index Page
 app.get('/', (req, res) => {
     // Variables for pug rendering
     let variables = {title: 'NF-Trainer', active_apps: true}
 
     // Render pug file at /views/3nf
     res.render(path + '3nf', variables)
+})
+
+// Start Trainer
+app.post('/checkFirstNfTask', (req, res, next) => {
+    let variables = {title: 'NF-Trainer', active_apps: true}
+    let done = () => res.render(path + 'checkFirstNfTask', variables)
+
+    let counter = 0
+    let number_functions = 3
+
+    // getTask
 })
 
 
