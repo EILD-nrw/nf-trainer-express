dfaa43ad0943aac16b5f390e744509df64625763
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-03-01 16:56:27 +0100

    getTask now returns the row instead of an row array

--- app/src/database.js
+++ app/src/database.js
@@ -12,25 +12,20 @@ const pool = new Pool({
 /**
  * Helper Function to get a certain task from the database
  *
- * @param task_id ID of the current task
+ * @param taskId ID of the current task
  * @param language Language of the task ('de' for german, everything else returns english)
  * @returns {Promise<number>} Returns 'Aufgabentext' and 'NF' from the Database
  */
-async function get_task(task_id, language) {
-    if (language === 'de') {
-        let { rows } = await pool.query('SELECT AUFGABENTEXT, NF FROM AUFGABEN WHERE ID = $1', task_id)
-        return rows
-    } else {
-        let { rows } = await pool.query('SELECT AUFGABENTEXT, NF FROM AUFGABEN_EN WHERE ID = $1', task_id)
-        return rows
-    }
+async function getTask(taskId, language) {
+        if (language === 'de') {
+            let { rows } = await pool.query('SELECT AUFGABENTEXT, NF FROM AUFGABEN WHERE ID = $1', [taskId])
+            return rows[0]
+        } else {
+            let { rows } = await pool.query('SELECT AUFGABENTEXT, NF FROM AUFGABEN_EN WHERE ID = $1', [taskId])
+            return rows[0]
+        }
 }
 
 export {
     get_task,
 }
-
-
-
-
-
