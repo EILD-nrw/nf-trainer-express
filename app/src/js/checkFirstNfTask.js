882c97cf37ef4c0e316b7bd7f55463d2ae9ad9ee
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-03-01 17:05:28 +0100

    Cleanup

--- app/src/js/checkFirstNfTask.js
+++ app/src/js/checkFirstNfTask.js
@@ -1,15 +1,17 @@
 $('#3nf_checkFirstNfTask').on('submit', function(e) {
+    let firstNfInput = $('input[name="firstNf"]');
 
-    if($('input[name="firstNf"]')[1].checked == false && $('input[name="firstNf"]')[0].checked == false){
+    if(firstNfInput[1].checked === false && firstNfInput[0].checked === false){
         alert("Bitte wählen Sie eine Antwort aus!");
         e.preventDefault();
 
     } else {
 
-        if(($('#valueHolder').attr('value') == 1 && $('input[name="firstNf"]')[1].checked == true
-            || ($('#valueHolder').attr('value') == 0 && $('input[name="firstNf"]')[0].checked == true))) {
+        let valueHolder = $('#valueHolder')
+        if((valueHolder.attr('value') === 1 && firstNfInput[1].checked === true
+            || (valueHolder.attr('value') === 0 && firstNfInput[0].checked === true))) {
 
-            console.log($('#valueHolder').attr('value'));
+            console.log(valueHolder.attr('value'));
 
             alert("Ihre Antwort ist falsch! Bitte überprüfen Sie ihre Antwort!");
             e.preventDefault();
