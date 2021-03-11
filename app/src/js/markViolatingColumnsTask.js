52b94408f8bb487fd7d4bd2e6c5a4a14f2f19e6f
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-03-11 01:03:36 +0100

    Reformatted

--- app/src/js/markViolatingColumnsTask.js
+++ app/src/js/markViolatingColumnsTask.js
@@ -2,24 +2,24 @@ var eventTargets = [];
 
 // target elements with the "draggable" class
 interact('.draggable')
-  .draggable({
-    // enable inertial throwing
-    inertia: true,
-    // keep the element within the area of it's parent
-    restrict: {
-      restriction: "parent",
-      endOnly: true,
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    },
-    // enable autoScroll
-    autoScroll: true,
+    .draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        restrict: {
+            restriction: "parent",
+            endOnly: true,
+            elementRect: {top: 0, left: 0, bottom: 1, right: 1}
+        },
+        // enable autoScroll
+        autoScroll: true,
 
-    // call this function on every dragmove event
-    onmove: dragMoveListener,
-    // call this function on every dragend event
-  });
+        // call this function on every dragmove event
+        onmove: dragMoveListener,
+        // call this function on every dragend event
+    });
 
-  function dragMoveListener (event) {
+function dragMoveListener(event) {
     var target = event.target,
         // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
@@ -27,69 +27,69 @@ interact('.draggable')
 
     // translate the element
     target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+        target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
 
     // update the posiion attributes
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
-  }
+}
 
 interact('.dropzone').dropzone({
-  // only accept elements matching this CSS selector
-  accept: '#yes-drop',
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.75,
+    // only accept elements matching this CSS selector
+    accept: '#yes-drop',
+    // Require a 75% element overlap for a drop to be possible
+    overlap: 0.75,
 
-  // listen for drop related events:
-  ondropactivate: function (event) {
-    // add active dropzone feedback
-    event.target.classList.add('drop-active');
-  },
-  ondragenter: function (event) {
-    var draggableElement = event.relatedTarget,
-        dropzoneElement = event.target;
+    // listen for drop related events:
+    ondropactivate: function (event) {
+        // add active dropzone feedback
+        event.target.classList.add('drop-active');
+    },
+    ondragenter: function (event) {
+        var draggableElement = event.relatedTarget,
+            dropzoneElement = event.target;
 
-    // feedback the possibility of a drop
-    dropzoneElement.classList.add('drop-target');
-    draggableElement.classList.add('can-drop');
+        // feedback the possibility of a drop
+        dropzoneElement.classList.add('drop-target');
+        draggableElement.classList.add('can-drop');
 
-    // draggableElement.textContent = 'Dragged in';
-  },
-  ondragleave: function (event) {
-    // remove the drop feedback style
-    event.target.classList.remove('drop-target');
-    event.relatedTarget.classList.remove('can-drop');
-    // event.relatedTarget.textContent = 'Dragged out';
+        // draggableElement.textContent = 'Dragged in';
+    },
+    ondragleave: function (event) {
+        // remove the drop feedback style
+        event.target.classList.remove('drop-target');
+        event.relatedTarget.classList.remove('can-drop');
+        // event.relatedTarget.textContent = 'Dragged out';
 
-    for(i = 0; i < eventTargets.length; i++){
-        if(event.relatedTarget.textContent == eventTargets[i]){
-            eventTargets.splice(i, 1);
+        for (i = 0; i < eventTargets.length; i++) {
+            if (event.relatedTarget.textContent == eventTargets[i]) {
+                eventTargets.splice(i, 1);
+            }
         }
-    }
-  },
-  ondrop: function (event) {
+    },
+    ondrop: function (event) {
 
-    enthalten = false;
+        enthalten = false;
 
-    for(i = 0; i < eventTargets.length; i++){
-        if(event.relatedTarget.textContent == eventTargets[i]){
-            enthalten = true;
+        for (i = 0; i < eventTargets.length; i++) {
+            if (event.relatedTarget.textContent == eventTargets[i]) {
+                enthalten = true;
+            }
         }
-    }
-    if(!enthalten){
-        eventTargets.push(event.relatedTarget.textContent);
-    }
-  },
+        if (!enthalten) {
+            eventTargets.push(event.relatedTarget.textContent);
+        }
+    },
 
-  ondropdeactivate: function (event) {
-    // remove active dropzone feedback
-    event.target.classList.remove('drop-active');
-    event.target.classList.remove('drop-target');
-  }
+    ondropdeactivate: function (event) {
+        // remove active dropzone feedback
+        event.target.classList.remove('drop-active');
+        event.target.classList.remove('drop-target');
+    }
 });
 
-$('#3nf_markViolatingColumnsTask').on('submit', function(e) {
+$('#3nf_markViolatingColumnsTask').on('submit', function (e) {
 
     var solutionArray = $('#valueHolderSolution').attr('value').split(';');
 
@@ -98,25 +98,25 @@ $('#3nf_markViolatingColumnsTask').on('submit', function(e) {
 
     solutionArray.forEach(element => {
 
-         for(i = 0; i < eventTargets.length; i++) {
+        for (i = 0; i < eventTargets.length; i++) {
 
-             if(element.toLowerCase() == eventTargets[i].toLowerCase()){
+            if (element.toLowerCase() == eventTargets[i].toLowerCase()) {
 
-                 rightCounter++;
+                rightCounter++;
 
-             } else if (element.toLowerCase() != eventTargets[i].toLowerCase()) {
+            } else if (element.toLowerCase() != eventTargets[i].toLowerCase()) {
 
-                 wrongCounter++;
+                wrongCounter++;
 
-             }
-        
-         }
+            }
 
-     });
+        }
 
-    wrongCounter = wrongCounter-((solutionArray.length -1)*(solutionArray.length -1))
+    });
 
-    if(rightCounter == solutionArray.length-1 && wrongCounter == 0){
+    wrongCounter = wrongCounter - ((solutionArray.length - 1) * (solutionArray.length - 1))
+
+    if (rightCounter == solutionArray.length - 1 && wrongCounter == 0) {
 
         alert("Ihre Antwort ist korrekt!");
 
@@ -127,5 +127,5 @@ $('#3nf_markViolatingColumnsTask').on('submit', function(e) {
         e.preventDefault();
 
     }
-    
+
 });
